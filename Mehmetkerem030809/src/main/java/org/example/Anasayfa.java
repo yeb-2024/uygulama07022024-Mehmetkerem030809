d3006135e52aa9dea213bd853c60d99d9625e741
Mehmet Kerem Özkan <mehmetkeremozkan@gmail.com>
2025-02-07 18:33:48 +0300

    Created Main Page

---
+++ Mehmetkerem030809/src/main/java/org/example/Anasayfa.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Anasayfa {
+    JFrame frame = new JFrame();
+
+    public Anasayfa(){
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle("Anasayfa");
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
